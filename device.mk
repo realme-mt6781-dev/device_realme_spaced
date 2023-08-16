c18a7a7e35ff56d5b8c3d404c2672a9f2bfd025f
bengris32 <bengris32@protonmail.ch>
2023-08-16 20:08:52 +0100

    spaced: Refactor cgroup setup
    Change-Id: I76a3eb3cd981f592055f034e9f09cd3b7d3f30d2
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -381,10 +381,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.mt6781 \
     fstab.mt6781_ramdisk \
-    init.cgroup.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6781.rc \
+    init.mt6781.power.rc \
     init.mt6781.usb.rc \
     init.project.rc \
     init.sensor_1_0.rc \
