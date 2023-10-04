b3807071700a78f20e7ba0406cada73adb8b6fee
Tim Zimmermann <tim@linux4.de>
2023-10-04 19:47:23 +0530

    spaced: Patch widevine to load libprotobuf-cpp-full-3.9.1
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,9 @@ function blob_fixup {
         vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
             sed -i 's/start/enable/' "$2"
             ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
