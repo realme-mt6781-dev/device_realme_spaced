f073d5db1b37faf7a8941ebefe974ec77407eb21
HELLINFIX <mridulsharma57570@gmail.com>
2023-10-18 14:32:31 +0000

    spaced: Build `libstagefright_foundation` from source
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     libstagefright_softomx_plugin.vendor \
     libsfplugin_ccodec_utils.vendor \
-    libcodec2_soft_common.vendor
+    libcodec2_soft_common.vendor \
+    libstagefright_foundation
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
